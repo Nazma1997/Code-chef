0c81ba4de03cd1a469a0776b6d7b4e18ce240bfb
nazma1997 <nazma.ist.707@gmail.com>
2025-05-24 17:45:52 +0600

    Solve difficulty 135

---
+++ difficulty-135.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+    cin >> x;
+    cin >> y;
+
+    if (y > x ||  x == y)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
