8736aace3ce1daa40a386f647e07bffe7f89d1fe
nazma1997 <nazma.ist.707@gmail.com>
2025-05-24 18:23:33 +0600

    Solve difficulty 213

---
+++ difficulty-213.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string x;
+    cin >> x;
+
+    for (int i = 0; i < x.length(); i++) {
+      
+        if (i == 6) {
+            cout << x[i] << endl;
+        }
+    }
+
+    return 0;
+}
